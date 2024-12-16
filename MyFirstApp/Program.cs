030f114ec81978b29a280232f61835aef2ba699d
Triin <triin.varblane@gmail.com>
2024-12-17 00:08:10 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// 
+Console.WriteLine("Enter your name"); //Output
+//string - sõne
+string userName;
+userName = Console.ReadLine(); //Input
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
